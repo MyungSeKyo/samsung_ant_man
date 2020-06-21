b110a5cb50967c61553aca4ee7deca3a251b05c6
Choe Dong Heon <ehdgjs357@naver.com>
2020-06-21 18:33:14 +0900

    add run evaluation script

---
+++ samsung_ant_man/scripts/run_evaluation.py
@@ -0,0 +1,24 @@
+from stocks.models import DailyDocument
+from core.ir_analyzer import analyze_doc
+
+
+def run():
+    tp, fp, fn, tn = 0, 0, 0, 0
+    for daily_doc in DailyDocument.objects.all():
+        score = analyze_doc(daily_doc.doc).get('score')
+        if str(daily_doc) == 'True':
+            if score > 0:
+                tp+=1
+            else:
+                fn+=1
+        else:
+            if score>0:
+                fp+=1
+            else:
+                tn+=1
+
+    P = tp / (tp+fp)
+    R = tp / (tp+fn)
+
+    F = (2*P*R) / (P+R)
+    print(F)
