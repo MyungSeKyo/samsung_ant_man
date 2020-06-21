ea72b8b1494c297c0bdf2b93d80a57d01d46c446
myungsegyo <myungsekyo@gmail.com>
2020-06-21 17:17:34 +0900

    add keyword search view url template

--- samsung_ant_man/stocks/urls.py
+++ samsung_ant_man/stocks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from stocks.views import IndexView
+from stocks.views import IndexView, AnalyzeDoc
 
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('analyze/', AnalyzeDoc.as_view(), name='analyze')
 ]
